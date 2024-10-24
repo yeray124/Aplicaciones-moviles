9502521f5fb5dd8978731db040b56b61e3d28b82
yeray124 <yeray0451@gmail.com>
2024-10-24 21:26:20 +0200

    cambios ej3

--- Practica_5/app/src/main/java/com/example/practica_5/MainActivity.java
+++ Practica_5/app/src/main/java/com/example/practica_5/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.practica_5;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,32 +16,54 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button Continuar;
     private EditText CajaContrasena;
+    private EditText CajaTextoCorreo;
+    private TextView MostrarInfo;
+    private Switch Recordar;
+    private TextView TextoRecordar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.diseno);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         CajaContrasena = findViewById(R.id.CajaContrasena);
-        Continuar = findViewById(R.id.Continuar);
+        CajaTextoCorreo = findViewById(R.id.CajaTextoCorreo);
+        MostrarInfo = findViewById(R.id.MostrarInfo);
+        MostrarInfo = findViewById(R.id.MostrarInfo);
+        Recordar = findViewById(R.id.Recordar);
+
+        MostrarInfo.setVisibility(View.INVISIBLE);
         Continuar.setOnClickListener(this);
     }
 
     @Override
-    public void  setOnClick(View view) {
-        if(view.getId()== R.id.Continuar && )
-        {
+    public void onClick(View view) {
+        String usuario = CajaTextoCorreo.getText().toString();
+        String contrasena = CajaContrasena.getText().toString();
+        String usuarioCorrecto = "correo@correo.com";
+        String contrasenaCorrecta = "123";
 
-        } else {
-
+        if (view.getId() == R.id.Continuar) {
+            if (usuario.equals(usuarioCorrecto) && contrasena.equals(contrasenaCorrecta)) {
+                MostrarInfo.setVisibility(View.VISIBLE);
+                MostrarInfo.setTextColor(getColor(R.color.verde));
+                MostrarInfo.setText("Inicio de sesion correcto :)");
+                if (Recordar.isChecked()) {
+                    MostrarInfo.setText(MostrarInfo.getText() + "\n" + "Almacenado para siguientes accesos");
+                }
+            } else {
+                MostrarInfo.setVisibility(View.VISIBLE);
+                MostrarInfo.setTextColor(getColor(R.color.rojo));
+                MostrarInfo.setText("Inicio de sesion erroneo :(");
+            }
         }
-
     }
-
-
 }
