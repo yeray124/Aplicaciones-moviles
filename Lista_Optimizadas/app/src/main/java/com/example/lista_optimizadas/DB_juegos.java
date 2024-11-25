e6cf88f4e2e6bf4840af27e5173f007e3885749f
yeray124 <yeray0451@gmail.com>
2024-11-25 15:58:29 +0100

    2

---
+++ Lista_Optimizadas/app/src/main/java/com/example/lista_optimizadas/DB_juegos.java
@@ -0,0 +1,9 @@
+package com.example.lista_optimizadas;
+
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB_juegos extends SQLiteOpenHelper {
+    private static final String DABASE_NAME = "DBJuegos.db";
+
+}
--- Lista_Optimizadas/app/src/main/java/com/example/lista_optimizadas/MainActivity.java
+++ Lista_Optimizadas/app/src/main/java/com/example/lista_optimizadas/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lista_optimizadas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,12 @@ import androidx.recyclerview.widget.RecyclerView;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private AdaptadorVideojuegos adaptador;
+    private Videojuegos[] listaVideojuegos;
+
+    private EditText NombreID;
+    private EditText TextUrl;
+    private Button buttonInsertar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +35,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
         recyclerView = findViewById(R.id.recView);
+        NombreID = findViewById(R.id.NombreID);
+        TextUrl = findViewById(R.id.TextUrl);
+        buttonInsertar = findViewById(R.id.buttonInsertar);
 
         Videojuegos[] videojuegos = new Videojuegos[2];
         videojuegos[0] = new Videojuegos("Elden ring", "elden", "2022-02-25");
         videojuegos[1] = new Videojuegos("Mario Bros", "mario", "1985-09-13");
 
+
         AdaptadorVideojuegos adaptador = new AdaptadorVideojuegos(videojuegos, this);
 
+        adaptador = new AdaptadorVideojuegos(listaVideojuegos, this);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adaptador);
 
+        buttonInsertar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = NombreID.getText().toString();
+                String url = TextUrl.getText().toString();
+
+                if (!nombre.isEmpty() && !url.isEmpty()) {
+                    insertarNuevoVideojuego(nombre, url);
+                } else {
+
+                    System.out.println("Error: campos vacíos :))))");
+                }
+            }
+        });
+    }
+
+    private void insertarNuevoVideojuego(String nombre, String url) {
+        Videojuegos nuevoVideojuego = new Videojuegos(nombre, url, "2023-11-25");
+
+        Videojuegos[] nuevaLista = new Videojuegos[listaVideojuegos.length + 1];
+        System.arraycopy(listaVideojuegos, 0, nuevaLista, 0, listaVideojuegos.length);
+        nuevaLista[listaVideojuegos.length] = nuevoVideojuego;
+        listaVideojuegos = nuevaLista;
+
+        adaptador = new AdaptadorVideojuegos(listaVideojuegos, this);
         recyclerView.setAdapter(adaptador);
     }
 }
