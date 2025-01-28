902ab2b761f6ac9be08bbaf1fe0d238bffa7e17a
yeray124 <yeray0451@gmail.com>
2025-01-28 19:51:21 +0100

    Varioas cambios funciona todo menos marcadores
    c1

--- Practica9/app/src/main/java/com/example/practica9/MainActivity.java
+++ Practica9/app/src/main/java/com/example/practica9/MainActivity.java
@@ -30,9 +30,8 @@ import org.osmdroid.views.overlay.Marker;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    
+
     private WebView video;
-    private Button buscar;
     private EditText id;
     private TextView nombreMonumento;
     private TextView descripcion;
@@ -75,45 +74,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MapController mapController = (MapController) mapa.getController();
 
 
-        GeoPoint startPoint = new GeoPoint(37.1773, -3.5986);
+        GeoPoint startPoint = new GeoPoint( 37.0245, -3.6236);
         mapa.getController().setCenter(startPoint);
         mapa.getController().setZoom(15.0);
 
 
         Configuration.getInstance().setUserAgentValue("appIdMapPractice");
-        ControladorMonumento controlador = new ControladorMonumento(getApplicationContext());
+        ControladorMonumento controlador = new ControladorMonumento(this);
     }
+
     @Override
     public void onClick(View view) {
 
-        String monumentId = id.getText().toString();
 
-            try {
-                controlador.obtenerMonumentoID(monumentId, new VolleyCallBack() {
-                    @Override
-                    public void onSuccess(Context context, ArrayList<Monumento> monumentos) {
-                        Monumento monumento = monumentos.get(0);
+        try {
+            controlador.obtenerTodosMonumentos(new VolleyCallBack() {
+                @Override
+                public void onSuccess(Context context, ArrayList<Monumento> monumentos) {
 
-                        caja.setVisibility(View.VISIBLE);
-                        //MOSTRAR INFORMACION DEL CONTROLADOR
+                    for (Monumento monumento : monumentos) {
+                        double Latitud = Double.valueOf(monumento.latitud);
+                        double Longitud = Double.valueOf(monumento.longitud);
 
-                        nombreMonumento.setText(monumento.nombre);
-                        fecha.setText("Construido: " + monumento.fecha);
-                        Picasso.get().load(monumento.imagen).into(imagen);
-                        descripcion.setText(monumento.descripcion);
-                        latitud.setText("Latitud: " + monumento.latitud);
-                        longitud.setText("Longitud: " + monumento.longitud);
-                        ubi.setText(monumento.ciudad);
-                        comprar.setText("Comprar tu entrada desde: " + monumento.precio + "€");
-                        String html = monumento.video;
-                        WebSettings settings = video.getSettings();
-                        settings.setJavaScriptEnabled(true);
-                        video.loadData(html, "text/html", "UTF-8");
+                        GeoPoint puntero = new GeoPoint(Latitud, Longitud);
+                        Marker marcador = new Marker(mapa);
+                        marcador.setPosition(puntero);
+
+                        mapa.getOverlays().add(marcador);
+                        mapa.invalidate();
+
+                        marcador.setOnMarkerClickListener((marker1, mapa) -> {
+                            ViewGroup.LayoutParams params = mapa.getLayoutParams();
+                            params.height = 500;
+
+                            mapa.setLayoutParams(params);
+
+                            caja.setVisibility(View.VISIBLE);
+
+
+                            // MOSTRAR INFORMACIÓN DEL CONTROLADOR
+                            nombreMonumento.setText(monumento.nombre);
+                            fecha.setText("Construido: " + monumento.fecha);
+                            Picasso.get().load(monumento.imagen).into(imagen);
+                            descripcion.setText(monumento.descripcion);
+                            latitud.setText("Latitud: " + monumento.latitud);
+                            longitud.setText("Longitud: " + monumento.longitud);
+                            ubi.setText(monumento.ciudad);
+                            comprar.setText("Comprar tu entrada desde: " + monumento.precio + "€");
+
+                            String html = monumento.video;
+                            WebSettings settings = video.getSettings();
+                            settings.setJavaScriptEnabled(true);
+                            video.loadData(html, "text/html", "UTF-8");
+
+                            return true;
+                        });
                     }
-                });
-            } catch (ServidorPHPException e) {
-                throw new RuntimeException(e);
-            }
+                }
+            });
+        } catch (ServidorPHPException e) {
+            throw new RuntimeException(e);
         }
-
+    }
 }
